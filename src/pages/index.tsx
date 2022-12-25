d191bb5d8917aa17250aa7d3bb35e6b1fa5c39f2
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2022-12-26 00:09:10 +0530

    feat: add about us section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
+import Button from "@/components/Button";
 import Hero from "@/components/Hero";
+import type { NextPage } from "next";
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <>
-      <main>
-        <Hero title="Empowering Youth for the Future" getInTouch={true} />
-      </main>
+      <Hero title="Empowering Youth for the Future" getInTouch={true} />
+      <div id="more" className="relative px-6 lg:px-8">
+        <div className="mx-auto max-w-4xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+          <div>
+            <div data-aos="fade-up">
+              <h1 className="pb-4 text-center text-lg font-medium tracking-tight sm:text-2xl">
+                WHO ARE WE?
+              </h1>
+              <h2 className="mt-6 text-center text-2xl text-zinc-600 sm:text-4xl">
+                <b>SAMARTH</b>, the educational society of Techno Main Salt
+                Lake, founded in 2019. <b>SAMARTH</b> literally means competent,
+                strong and powerful.
+              </h2>
+              <div className="mt-8 flex justify-center gap-x-4">
+                <Button variant="primary" bouncy={true} shadow="medium">
+                  <a href="#aim">
+                    Our Aim
+                    <span className="pl-1 text-orange-100" aria-hidden="true">
+                      &darr;
+                    </span>
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
