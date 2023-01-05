1c33bb25678cecdf6759c32c0b8ad062c2eab9ed
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-05 22:16:25 +0530

    feat: add our aim section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import Button from "@/components/Button";
 import Hero from "@/components/Hero";
+import {
+  Cog6ToothIcon,
+  LifebuoyIcon,
+  LightBulbIcon,
+  PaintBrushIcon,
+} from "@heroicons/react/24/outline";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
@@ -27,6 +33,88 @@ const Home: NextPage = () => {
                     </span>
                   </a>
                 </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="aim" className="relative px-6 sm:-scroll-m-20 lg:px-8">
+        <div className="mx-auto max-w-6xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+          <div>
+            <div>
+              <div data-aos="fade-up" className="mx-auto max-w-4xl">
+                <h1 className="pb-4 text-center text-lg font-medium tracking-tight sm:text-2xl">
+                  OUR AIM
+                </h1>
+                <h1 className="text-center text-4xl font-bold tracking-tight sm:text-5xl">
+                  Empowering the Youth
+                </h1>
+                <p className="mt-6 text-center text-lg leading-8 text-zinc-600">
+                  Building self confidence. Work in the direction of self growth
+                  and transformation. Organizing various kinds of events like
+                  extempore, Group discussion,mock interview,etc. Holistic
+                  development of an individual through :
+                </p>
+              </div>
+
+              <div className="mt-8 flex flex-wrap space-y-6 sm:space-y-0">
+                <div data-aos="zoom-in" className="sm:p-4 md:w-1/2 xl:w-1/4">
+                  <div className="h-full rounded-lg border border-zinc-200 p-6 transition-all duration-200 ease-in-out hover:shadow-xl">
+                    <div className="mb-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-sky-100 text-sky-500">
+                      <LightBulbIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <h2 className="mb-2 text-lg font-medium text-zinc-900">
+                      Strategy
+                    </h2>
+                    <p className="text-base leading-relaxed text-zinc-500">
+                      A strategy describes how the ends (goals) will be achieved
+                      by the means (resources).
+                    </p>
+                  </div>
+                </div>
+                <div data-aos="zoom-in" className="sm:p-4 md:w-1/2 xl:w-1/4">
+                  <div className="h-full rounded-lg border border-zinc-200 p-6 transition-all duration-200 ease-in-out hover:shadow-xl">
+                    <div className="mb-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-rose-100 text-rose-500">
+                      <PaintBrushIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <h2 className="mb-2 text-lg font-medium text-zinc-900">
+                      Design
+                    </h2>
+                    <p className="text-base leading-relaxed text-zinc-500">
+                      Graphic design is the craft of planning and creating
+                      visual content to communicate ideas and messages.
+                    </p>
+                  </div>
+                </div>
+                <div data-aos="zoom-in" className="sm:p-4 md:w-1/2 xl:w-1/4">
+                  <div className="h-full rounded-lg border border-zinc-200 p-6 transition-all duration-200 ease-in-out hover:shadow-xl">
+                    <div className="mb-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-amber-100 text-amber-500">
+                      <Cog6ToothIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <h2 className="mb-2 text-lg font-medium text-zinc-900">
+                      Grooming & Development
+                    </h2>
+                    <p className="text-base leading-relaxed text-zinc-500">
+                      Grooming is the secret of real elegance. We believe in
+                      learning by action!
+                    </p>
+                  </div>
+                </div>
+                <div data-aos="zoom-in" className="sm:p-4 md:w-1/2 xl:w-1/4">
+                  <div className="h-full rounded-lg border border-zinc-200 p-6 transition-all duration-200 ease-in-out hover:shadow-xl">
+                    <div className="mb-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-emerald-100 text-emerald-500">
+                      <LifebuoyIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <h2 className="mb-2 text-lg font-medium text-zinc-900">
+                      Help & Supports
+                    </h2>
+                    <p className="text-base leading-relaxed text-zinc-500">
+                      Support plays a huge role in any journey. &quot;A mentor
+                      is someone who allows you to see the hope within
+                      yourself&quot;.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
