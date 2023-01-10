04575ee84c1cf910655ff2560e866eb2bd3ed18d
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-11 04:01:47 +0530

    feat: add footer component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/layout/Navbar";
+import Footer from "@/layout/Footer";
 
 export default function Layout({ children }: any): JSX.Element {
   return (
     <div>
       <Navbar />
       <main>{children}</main>
+      <Footer/>
     </div>
   );
 }
