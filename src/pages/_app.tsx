eaf0039eca1757808207b57c2a58ba70cab8389f
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2023-01-11 03:42:22 +0530

    feat: initialize aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import Layout from "@/layout/index";
+import "@/styles/globals.css";
+import AOS from "aos";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      easing: "ease-in-out",
+    });
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
